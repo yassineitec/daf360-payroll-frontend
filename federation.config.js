1ef32ba72ad62b6cf0e15d72af4dd979fe8c73f1
khalil rebhi <khalil.rebhi@itecgroupe.com>
2026-08-20 17:46:01 +0100

    chore: update @khalilrebhiitec/daf360 dependency version to ^4.21.0 and optimize Dockerfile for caching

--- federation.config.js
+++ federation.config.js
@@ -45,7 +45,7 @@ module.exports = withNativeFederation({
     '@khalilrebhiitec/daf360': {
       singleton: true,
       strictVersion: false,
-      requiredVersion: '^4.19.0',
+      requiredVersion: '^4.21.0',
     },
     '@ngrx/store': {
       singleton: true,
