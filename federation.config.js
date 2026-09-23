fe83534a89669870aceb7f8abb5203c25536f22e
khalil rebhi <khalil.rebhi@itecgroupe.com>
2026-09-23 17:20:28 +0100

    chore: update @khalilrebhiitec/daf360 dependency version to ^5.0.0 in package.json, package-lock.json, and federation.config.js

--- federation.config.js
+++ federation.config.js
@@ -45,7 +45,7 @@ module.exports = withNativeFederation({
     '@khalilrebhiitec/daf360': {
       singleton: true,
       strictVersion: false,
-      requiredVersion: '^4.33.0',
+      requiredVersion: '^5.0.0',
     },
     '@ngrx/store': {
       singleton: true,
